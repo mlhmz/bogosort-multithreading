9d2b4830e364080f0daf3a22fb0b8d45066412e6
Malek Hamza <malek.hamza@gmx.de>
2023-10-18 14:47:45 +0200

    Add cheap multithreading cli

--- src/main/java/xyz/mlhmz/bogosort/Bogosort.java
+++ src/main/java/xyz/mlhmz/bogosort/Bogosort.java
@@ -1,6 +1,8 @@
 package xyz.mlhmz.bogosort;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Bogosort {
@@ -27,12 +29,26 @@ public class Bogosort {
             thread.start();
         }
         while (true) {
-            if (reference.get() != null) {
-                threadList.forEach(Thread::interrupt);
-                return reference.get();
+            System.out.println("Multithreaded Bogosort");
+            System.out.println("As soon as a benchmark result appears, a sort was finished and a stop can be triggered");
+            System.out.println("Commands:");
+            System.out.println("stop - Ends all threads and gets the reference result");
+            System.out.println("info - Shows all threads information");
+            String input = new Scanner(System.in).next();
+            switch (input) {
+                case "stop" -> {
+                    threadList.forEach(Thread::interrupt);
+                    return reference.get();
+                }
+                case "info" -> printThreadInformation(threadList);
+                default -> System.out.println("Unknown commands. Available commands: 'stop', 'info'.");
             }
-            Thread.sleep(100);
         }
+    }
+
+    private void printThreadInformation(List<Thread> threadList) {
+        System.out.println("Thread Name\t\tThread State");
+        threadList.forEach(thread -> System.out.printf("%s\t\t%s%n", thread.getName(), thread.getState().toString()));
     }
 
     public int[] sort(int[] array) {
